7b4e9f9c41b4da2e6032895597aa0e4399d759d7
gustavoqooo <gustavoqooo20@gmail.com>
2024-06-01 20:02:53 -0500

    controladores terminados

--- src/main/java/pe/edu/cibertec/app_security_backoffice/controller/SeguridadController.java
+++ src/main/java/pe/edu/cibertec/app_security_backoffice/controller/SeguridadController.java
@@ -1,4 +1,61 @@
 package pe.edu.cibertec.app_security_backoffice.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.app_security_backoffice.model.bd.Usuario;
+import pe.edu.cibertec.app_security_backoffice.model.dto.ResultadoDto;
+import pe.edu.cibertec.app_security_backoffice.model.dto.UsuarioDto;
+import pe.edu.cibertec.app_security_backoffice.service.UsuarioService;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/seguridad")
 public class SeguridadController {
+    private UsuarioService usuarioService;
+    @GetMapping("/usuario")
+    public String frmMantUsuario(Model model){
+        model.addAttribute("listaUsuarios",
+                usuarioService.listarUsuario());
+        return "seguridad/frmmantusuario";
+    }
+    @PostMapping("/usuario")
+    @ResponseBody
+    public ResultadoDto registrarUsuario(@RequestBody UsuarioDto usuarioDto){
+        String mensaje = "Usuario registrado correctamente";
+        boolean respuesta = true;
+        try {
+            Usuario usuario = new Usuario();
+            usuario.setNombres(usuarioDto.getNombres());
+            usuario.setApellidos(usuarioDto.getApellidos());
+            if(usuarioDto.getIdusuario() > 0){
+                usuario.setIdusuario(usuarioDto.getIdusuario());
+                usuario.setActivo(usuarioDto.getActivo());
+                usuarioService.actualizarUsuario(usuario);
+            }else{
+                usuario.setNomusuario(usuarioDto.getNomusuario());
+                usuario.setEmail(usuarioDto.getEmail());
+                usuarioService.guardarUsuario(usuario);
+            }
+        }catch (Exception ex){
+            mensaje = "Usuario no registrado, error en la BD";
+            respuesta = false;
+        }
+        return ResultadoDto.builder().mensaje(mensaje).respuesta(respuesta).build();
+    }
+
+    @GetMapping("/usuario/{id}")
+    @ResponseBody
+    public Usuario frmMantUsuario(@PathVariable("id") int id){
+        return usuarioService.buscarUsuarioXIdUsuario(id);
+    }
+    @GetMapping("/usuariosall")
+    @ResponseBody
+    public List<Usuario> listaUsuario(){
+        return usuarioService.listarUsuario();
+    }
+
 }
--- src/main/java/pe/edu/cibertec/app_security_backoffice/model/dto/ResultadoDto.java
+++ src/main/java/pe/edu/cibertec/app_security_backoffice/model/dto/ResultadoDto.java
@@ -1,7 +1,9 @@
 package pe.edu.cibertec.app_security_backoffice.model.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class ResultadoDto {
     private Boolean respuesta;
