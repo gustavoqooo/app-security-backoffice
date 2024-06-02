d5e6bf7a58487058ccd96b6e3feb975834f4ef1c
gustavoqooo <gustavoqooo20@gmail.com>
2024-06-01 19:22:21 -0500

    spring security finalizado

--- src/main/java/pe/edu/cibertec/app_security_backoffice/configuration/SecurityConfig.java
+++ src/main/java/pe/edu/cibertec/app_security_backoffice/configuration/SecurityConfig.java
@@ -1,4 +1,51 @@
 package pe.edu.cibertec.app_security_backoffice.configuration;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import pe.edu.cibertec.app_security_backoffice.service.DetalleUsuarioService;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private DetalleUsuarioService detalleUsuarioService;
+
+    public SecurityFilterChain config(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers("/auth/login",
+                                        "/resources/**",
+                                        "/static/**",
+                                        "/styles/**",
+                                        "/scripts/**")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated()
+                ).formLogin(
+                        login ->
+                                login.loginPage("/auth/login")
+                                        .defaultSuccessUrl("/auth/login-success")
+                                        .usernameParameter("nomusuario")
+                                        .passwordParameter("password")
+                ).logout(
+                        logout ->
+                                logout.logoutSuccessUrl("/auth/login")
+                                        .invalidateHttpSession(true)
+                ).authenticationProvider(authProvider());
+        return httpSecurity.build();
+    }
+    private AuthenticationProvider authProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(detalleUsuarioService);
+        authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return authenticationProvider;
+    }
 }
